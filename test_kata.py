8761124518daae06906db4ca62d8b954f0cdac9b
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-06-20 22:57:26 +0300

    no

--- test_kata.py
+++ test_kata.py
@@ -4,7 +4,7 @@ import unittest
 
 class Test_Pete_the_Baker(unittest.TestCase):
     def test0(self):    
-        self.assertEqual(snail(array), expected)
+        self.assertEqual(0, 1)
 
 
 if __name__ == '__main__':
