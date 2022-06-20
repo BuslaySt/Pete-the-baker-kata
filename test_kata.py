23bd82478a5889941e00e4d777e405a4206130d4
BuslaySt <71297919+BuslaySt@users.noreply.github.com>
2022-06-20 22:56:21 +0300

    making files

---
+++ test_kata.py
@@ -0,0 +1,11 @@
+from kata import *
+import unittest
+
+
+class Test_Pete_the_Baker(unittest.TestCase):
+    def test0(self):    
+        self.assertEqual(snail(array), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
